0a1c169b220de290ba83e6bbb14a894bbed6db96
FRANCESCO PAOLO DELLAQUILA <paolodellaquila1999@gmail.com>
2017-01-27 20:54:05 +0100

    write function of menu and DBMS

---
+++ Overtime.cpp
@@ -0,0 +1,147 @@
+//Informatic project Dellaquila #1
+
+/*realizzare un porgramma che:
+calcoli le ore di straordinario effettuate dai 10 dipendenti dell'azienda Creasis.
+I dipendenti sono tenuti a lavorare 40 ore settimanali in 5 giorni (DA LUNEDI A VENERDI) e ogni giorno badgiano all'entrata e all'uscita.
+Alla fine del mese(di 30 giorni)l'ufficio delle risorse umane, deve sapere quanto ore di straordinario deve pagare a ciascun dipendente.
+
+Tesina
+
+-traccia
+-analisi dei dati
+-definizione delle strutture dati
+-strategia risolutiva
+-codice 
+-output(videata)
+
+Termine:Mercoledì 2
+Linguaggio:C*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+//---------------------------structure--------------------------
+
+
+
+
+
+//-------------------------function------------------------------
+
+void DBMS_add(FILE *config){
+	
+	FILE *lista_operai;
+	char dato[100];
+	
+	//open in "a" mode to add workers
+	lista_operai=fopen("database_operai/lista_operai/lista_operai.txt","a");
+	
+	//read workers from setting
+	//write information on file
+	fprintf(lista_operai,"----------------------Number of workers: 0----------------------");
+}
+
+//function that simulate a DBMS (Database Management System) to manage all workers in industry
+void DBMS_menu(){
+	
+	int risposta,scelta=1,check_data=0,i;
+	char username[100],password[100],read_file[100];
+	
+	//load config.ini
+	FILE *config;
+	
+	//open config.ini
+	config = fopen("settings/config.ini","r");
+			
+	if(config == NULL){
+		printf("\nOps....you delete or move config.ini file. Please restart the program in the same folder of settings/config.ini");
+	}else{
+		//check if user is admin
+		printf("\nInsert admin Username: ");
+		scanf("%s",username);
+		printf("\nInsert admin Password: ");
+		scanf("%s",password);
+		while(fscanf(config,"%s",read_file) > 0){
+			if(strcmp(username,read_file) == 0){
+				printf("\n\nUsername check\n\n");
+				for(i=0;i<3;i++){
+					fscanf(config,"%s",read_file);
+					if(strcmp(password,read_file) == 0){
+						printf("\n\nPassword Check\n\n");
+						check_data=1;
+					}
+				}
+			}
+		}	
+		if(check_data!=1){
+			printf("\nUsername or password is incorrect.");
+		}else{
+			while(scelta!=0){
+				//clean screen
+				system("cls");
+				printf("\nWelcome Admin\n\n");
+				printf("\nChoose an action: \n");
+				printf("|1)Add Workers      |\n");
+				printf("|2)Delete Workers   |\n");
+				printf("|3)Search Workers   |\n");
+				printf("|4)Print  Workers   |\n");	
+				printf("\nInsert action: ");
+				scanf("%d",&risposta);
+				
+				switch (risposta){
+				
+					case 1: DBMS_add(config);
+					break;
+					
+					
+					default: "wrong action";
+				}
+				printf("\n\nContinue with DBMS?:(S=1/N=0) ");
+				scanf("%d",&scelta);
+				system("cls");
+			}
+		}
+	}
+}
+
+void menu (){
+	
+	int risposta,scelta=1;
+	
+	while(scelta!=0){
+		//clean screen
+		system("cls");
+		
+		printf("\n\nChoose an action: \n");
+		printf("---------------------|\n");
+		printf("|1)DBMS              |\n");
+		printf("|2)BADGE             |\n");
+		printf("---------------------|\n");
+		printf("|3)OVERTIME          |\n");
+		printf("\nInsert action: ");
+		scanf("%d",&risposta);
+		
+		switch (risposta){
+		
+			case 1: DBMS_menu();
+			break;
+			
+			default: "wrong action";
+		}
+		printf("\n\nContinue with Menu?:(S=1/N=0) ");
+		scanf("%d",&scelta);
+	}
+}
+//---------------------------main--------------------------------
+int main(){
+	
+	//initialize design
+	system("color 3F");
+	system("title Overtime Project by D.F.P");
+	printf("Welcome to Overtime Software");
+	
+	//call menu
+	menu();
+	
+}
